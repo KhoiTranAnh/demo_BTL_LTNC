2a7550eeab40361dc80753abf700e899107d10f5
KhoiTranAnh <trananhkhoitv@gmail.com>
2024-03-31 16:39:56 +0700

    demo login and sign-up pages

---
+++ Login_page/login.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import {
+  getDatabase,
+  get,
+  ref,
+  child,
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnW_Ro14CeZh3Azt4IOT7mmQUnvvagNmA",
+  authDomain: "fir-login-page-90957.firebaseapp.com",
+  databaseURL: "https://fir-login-page-90957-default-rtdb.firebaseio.com",
+  projectId: "fir-login-page-90957",
+  storageBucket: "fir-login-page-90957.appspot.com",
+  messagingSenderId: "376190556670",
+  appId: "1:376190556670:web:dab148673eb440d52588af",
+  measurementId: "G-BD1D7KXSV6",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase();
+const dbRef = ref(db);
+
+let email = document.getElementById("email-input");
+let password = document.getElementById("password-input");
+let mainForm = document.getElementById("main-form");
+
+let signInUser = (event) => {
+  event.preventDefault();
+
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      console.log(userCredential);
+      alert("Login Successfully");
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log(error.code);
+      console.log(error.message);
+    });
+};
+
+mainForm.addEventListener("submit", signInUser);
